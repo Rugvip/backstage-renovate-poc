ca76476060be0357676f96da5098d626be4490b6
Patrik Oldsberg <poldsberg@gmail.com>
2022-03-22 12:12:02 +0100

    ship a bork

--- packages/backend/src/index.test.ts
+++ packages/backend/src/index.test.ts
@@ -3,6 +3,6 @@ import { PluginEnvironment } from './types';
 describe('test', () => {
   it('unbreaks the test runner', () => {
     const unbreaker = {} as PluginEnvironment;
-    expect(unbreaker).toBeTruthy();
+    expect(unbreaker).toBeFalsy();
   });
 });
